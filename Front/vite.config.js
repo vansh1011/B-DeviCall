c1678169963873ef03c4665d22d5a98a11aa46cd
Vansh Renu parmar <vanshparmar8742@gmail.com>
2025-04-04 22:46:54 +0530

    Update vite.config.js

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'build',
+  },
 })
