af72376a679d320e4bdc1f7ef473ebd2aa203c6f
Vansh Renu parmar <vanshparmar8742@gmail.com>
2025-04-04 22:18:47 +0530

    third

--- Front/src/Context/AuthContext.jsx
+++ Front/src/Context/AuthContext.jsx
@@ -2,11 +2,11 @@ import { createContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import httpStatus from "http-status";
-
+import server from "../../../environment";
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "http://localhost:8000/api/v1/users",
+    baseURL: `${server}/api/v1/users`,
 });
 
 export const AuthProvider = ({ children }) => {
---
+++ environment.js
@@ -0,0 +1,7 @@
+let IS_PROD = true;
+const server = IS_PROD ? 
+    "https://b-devicall-back.onrender.com" :
+    "http://localhost:8000"
+
+
+export default server;
